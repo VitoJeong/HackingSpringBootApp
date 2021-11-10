4b096d09aa111ae71e640d967941bf61e070c66d
ChangHa <ckdghk987@gmail.com>
2021-11-10 21:17:04 +0900

    create: project

---
+++ src/main/java/com/greglturnquist/reactive/HackingSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackingSpringBootAppApplication.class, args);
+    }
+
+}
