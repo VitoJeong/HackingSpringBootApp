80073d4f33c3029ece3da4b435080fcb75de5e0b
ChangHa <ckdghk987@gmail.com>
2021-11-25 18:35:52 +0900

    add: ch3 module(왜 사라졌지?)

---
+++ ch3-reactive/src/main/java/com/greglturnquist/reactive/Ch3ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
+
+// @SpringBootApplication
+public class Ch3ReactiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3ReactiveApplication.class, args);
+    }
+}
