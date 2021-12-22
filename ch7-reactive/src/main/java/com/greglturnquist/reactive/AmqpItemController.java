ad5edec29b3c18271de01e720fd17dd8c5641810
ChangHa <ckdghk987@gmail.com>
2021-12-22 19:22:54 +0900

    add: RabbitTest, AmqpItemController

---
+++ ch7-reactive/src/main/java/com/greglturnquist/reactive/AmqpItemController.java
@@ -0,0 +1,45 @@
+package com.greglturnquist.reactive;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class AmqpItemController {
+
+    private final AmqpTemplate template;
+
+    @PostMapping("/items")
+    Mono<ResponseEntity<?>> addNewItemUsingSpringAmqp(@RequestBody Mono<Item> item) {
+        return item
+                // boundedElastic 이라는 별도의 스레드로 실행
+                // -> AmqpTemplate은 블로킹 API를 호출하기 때문
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(content -> {
+                    return Mono
+                            .fromCallable(() -> {
+                                // exchange, routing-key, body(Item 데이터)
+                                template.convertAndSend(
+                                        "hacking-spring-boot", "new-items-spring-amqp", content);
+
+                                // URI를 lcation 헤더에 담아 201 상태코드와 반환
+                                return ResponseEntity.created(URI.create("/items")).build();
+                            });
+                });
+    }
+
+    // 리액터 플로우에서 스케줄러를 변경하는 두가지 방법
+    // publishOn(): 호출되는 시점 이후로 지정한 스케줄러 사용. 여러번 바꿀 수 있음
+    // subscribeOn(): 플로우 전 단계에 걸치는 스케줄러 지정.
+}
---
+++ ch7-reactive/src/test/java/com/greglturnquist/reactive/RabbitTest.java
@@ -0,0 +1,79 @@
+package com.greglturnquist.reactive;
+
+import com.greglturnquist.reactive.repository.ItemRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@Testcontainers
+@ContextConfiguration
+public class RabbitTest {
+
+    @Container
+    static RabbitMQContainer container =
+            new RabbitMQContainer("rabbitmq:3.7.25-management-alpine"); // <5>
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemRepository repository;
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.rabbitmq.host", container::getContainerIpAddress);
+        registry.add("spring.rabbitmq.port", container::getAmqpPort);
+    }
+
+    @Test
+    @DisplayName("AMQP를 통해 Item을 저장 후 목록을 조회")
+    void verifyMessagingThroughAmqp() throws InterruptedException {
+        this.webTestClient.post().uri("/items")
+                .bodyValue(new Item("Alf alarm clock", "nothing important", 19.99)) //
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody();
+
+        Thread.sleep(1500L);
+
+        this.webTestClient.post().uri("/items")
+                .bodyValue(new Item("Smurf TV tray", "nothing important", 29.99)) //
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody();
+
+        Thread.sleep(2000L);
+
+        this.repository.findAll()
+                .as(StepVerifier::create)
+                .expectNextMatches(item -> {
+                    assertThat(item.getName()).isEqualTo("Alf alarm clock");
+                    assertThat(item.getDescription()).isEqualTo("nothing important");
+                    assertThat(item.getPrice()).isEqualTo(19.99);
+                    return true;
+                })
+                .expectNextMatches(item -> {
+                    assertThat(item.getName()).isEqualTo("Smurf TV tray");
+                    assertThat(item.getDescription()).isEqualTo("nothing important");
+                    assertThat(item.getPrice()).isEqualTo(29.99);
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+}
