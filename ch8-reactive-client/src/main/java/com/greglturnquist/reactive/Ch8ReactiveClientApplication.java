496c6685467149e2596e0f45e0a2fb4587acbe2c
ChangHa <ckdghk987@gmail.com>
2022-01-05 20:34:18 +0900

    add: ch8-reactive-client

---
+++ ch8-reactive-client/src/main/java/com/greglturnquist/reactive/Ch8ReactiveClientApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8ReactiveClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch8ReactiveClientApplication.class, args);
+    }
+
+}
---
+++ ch8-reactive-client/src/test/java/com/greglturnquist/reactive/Ch8ReactiveClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greglturnquist.reactive;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class Ch8ReactiveClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.info("start!");
+    }
+
+}
